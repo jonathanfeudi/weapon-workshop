be3ee4ce60349888c53018621301201840a9b82d
Jonathan Feudi <jonathanfeudi@gmail.com>
2016-02-29 14:49:12 -0500

    Fucking exclamation marks

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -19,6 +19,38 @@ for(var i = 0; i < stocks.length; i++){
   stocks[i].addEventListener('click', addSelected);
 };
 
+function renderEngineText(){
+  for(var i = 0; i < engines.length; i++){
+    if(engines[i].classList.contains('selected')){
+      document.getElementById('engineText').innerHTML = 'Damage type: ' + (JSON.parse(eval('engineArray' + i).innerHTML).dmgtype) + ' / Debuff: ' + (JSON.parse(eval('engineArray' + i).innerHTML).debuff);
+    }
+  }
+};
+
+function renderReceiverText(){
+  for(var i = 0; i < receivers.length; i++){
+    if(receivers[i].classList.contains('selected')){
+      document.getElementById('receiverText').innerHTML = 'Damage: ' + (JSON.parse(eval('receiverArray' + i).innerHTML).dmg) + ' / Rate of Fire: ' + (JSON.parse(eval('receiverArray' + i).innerHTML).rof);
+    }
+  }
+};
+
+function renderBarrelText(){
+  for(var i = 0; i < barrels.length; i++){
+    if(barrels[i].classList.contains('selected')){
+      document.getElementById('barrelText').innerHTML = 'Range: ' + (JSON.parse(eval('barrelArray' + i).innerHTML).range) + ' / Heat: ' + (JSON.parse(eval('barrelArray' + i).innerHTML).heat);
+    }
+  }
+};
+
+function renderStockText(){
+  for(var i = 0; i < stocks.length; i++){
+    if(stocks[i].classList.contains('selected')){
+      document.getElementById('stockText').innerHTML = 'Accuracy: ' + (JSON.parse(eval('stockArray' + i).innerHTML).accuracy) + ' / Drawtime: ' + (JSON.parse(eval('stockArray' + i).innerHTML).drawtime);
+    }
+  }
+};
+
 function addSelected(){
   var likeThis = document.getElementsByClassName(this.className);
   for(var i = 0; i < likeThis.length; i++){
@@ -27,4 +59,13 @@ function addSelected(){
   }
   this.classList.add('selected');
   this.removeEventListener('click', addSelected);
+  renderEngineText();
+  renderReceiverText();
+  renderBarrelText();
+  renderStockText();
 }
+
+renderEngineText();
+renderReceiverText();
+renderBarrelText();
+renderStockText();
